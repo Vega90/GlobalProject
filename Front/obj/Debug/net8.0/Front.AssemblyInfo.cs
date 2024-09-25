4938adcbcbf6524b78f78f87873bc10de2b70224
cpana <cpana@ANA-OMEN>
2024-09-25 19:11:09 +0200

    subidas las ultimas versiones de los modelos

--- Front/obj/Debug/net8.0/Front.AssemblyInfo.cs
+++ Front/obj/Debug/net8.0/Front.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625f3114996051ff0796163f11261cf140c8f190")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd296c0b4a1afad31150a172690f442cff1c416")]
 [assembly: System.Reflection.AssemblyProductAttribute("Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
